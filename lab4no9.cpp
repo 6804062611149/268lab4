5612ca3a36c7b23a85541b5b72a91d562257e663
6804062611149 <s6804062611149@email.kmutnb.ac.th>
2026-01-04 19:50:08 +0700

    Fixed No.9 (Forgot B grade)

--- lab4no9.cpp
+++ lab4no9.cpp
@@ -4,11 +4,17 @@ int main () {
 	int score;
 	for (;scanf("%d",&score) == 1 && score != -1;){
         if (score >= 68) {
-            if (score >= 85)      printf("%d(A)\n", score);
-            else                  printf("%d(C)\n", score);
+            if (score >= 85)
+				printf("%d(A)\n", score);
+            else if (score >= 75)
+				printf("%d(B)\n", score);
+            else
+				printf("%d(C)\n", score);
         } else {
-            if (score >= 55)      printf("%d(D)\n", score);
-            else                  printf("%d(F)\n", score);
+            if (score >= 55)
+				printf("%d(D)\n", score);
+            else
+				printf("%d(F)\n", score);
         }
     }
 }
